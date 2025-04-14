e5a4e0eb4e6ea546b89bb4efca26f19286419b06
FrankTagoe <ftagoe6414@stu.d214.org>
2025-04-14 10:26:29 -0500

    Diamonds array

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Cards : [String : Int] = ["Ace of Diamonds" : 1, "Two of Diamonds" : 2, "Three of Diamonds" : 3, "Four of Diamonds" : 4, "Five of Diamonds" : 5, "Six of Diamonds" : 6, "Seven of Diamonds": 7, "Eight of Diamonds" : 8, "Nine of Diamonds" : 9, "King of Diamonds" : 10, "Queen of Diamonds" : 10, "Jack of Diamonds" : 10 ]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
         }
     }
 }
