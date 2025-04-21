1041a9d1dbe53d986f698d399a30e90c484a638e
Spencer <sfung6124@stu.d214.org>
2025-04-21 11:02:04 -0500

    added hit code

--- Play.swift
+++ Play.swift
@@ -2,16 +2,19 @@ import SwiftUI
 
 struct PlayPage: View {
     @State var gameStarted = false
-     @State var gameOver = false
-     @State var resultMessage = ""
+    @State var gameOver = false
+    @State var resultMessage = ""
     @State private var playerCards: [Int] = []
     
     var playerScore: Int {
-            playerCards.reduce(0, +)
-        }
+        playerCards.reduce(0, +)
+    }
     
     var body: some View {
         VStack{
+                        Button("hit"){
+                            hit()
+                        }
             if gameStarted && !gameOver {
                 VStack(spacing: 20) {
                     Text("Score: \(playerScore)")
@@ -19,38 +22,49 @@ struct PlayPage: View {
                 }
             }
             if !gameStarted {
-                            Button("Start Game") {
-                                startGame()
-                            }
-                            .font(.title2)
-                            .padding()
-                            .background(Color.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(12)
-                        }
+                Button("Start Game") {
+                    startGame()
+                }
+                .font(.title2)
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+            }
             if gameOver {
-                         Text(resultMessage)
-                             .font(.title2)
-                             .foregroundColor(.red)
-
-                         Button("Play Again") {
-                             startGame()
-                         }
-                         .font(.title2)
-                         .padding()
-                         .background(Color.purple)
-                         .foregroundColor(.white)
-                         .cornerRadius(12)
-                     }
+                Text(resultMessage)
+                    .font(.title2)
+                    .foregroundColor(.red)
+                
+                Button("Play Again") {
+                    startGame()
+                }
+                .font(.title2)
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+            }
+        }
+        }
+        func hit() {
+            playerCards.append(drawCard())
+            checkForBust()
+        }
+        func checkForBust() {
+            
+            if playerScore > 21 {
+                resultMessage = "You busted! Score: \(playerScore)";             gameOver = true
+            }
+        }
+        func startGame() {
+            gameStarted = true
+            gameOver = false
+            resultMessage = ""
+            playerCards = [drawCard(), drawCard()]
+        }
+        func drawCard() -> Int {
+            Int.random(in: 2...11)
         }
     }
-    func startGame() {
-           gameStarted = true
-           gameOver = false
-           resultMessage = ""
-           playerCards = [drawCard(), drawCard()]
-       }
-    func drawCard() -> Int {
-          Int.random(in: 2...11)
-      }
-}
+
