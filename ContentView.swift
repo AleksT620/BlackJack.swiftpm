b7b1795a0974634b1babbd9210fe53d879448a63
Aleks Todorov <atodorov6421@stu.d214.org>
2025-04-23 10:55:42 -0500

    deleted old array

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Cards : [String : Int] = ["Ace of Diamonds" : 1, "Two of Diamonds" : 2, "Three of Diamonds" : 3, "Four of Diamonds" : 4, "Five of Diamonds" : 5, "Six of Diamonds" : 6, "Seven of Diamonds": 7, "Eight of Diamonds" : 8, "Nine of Diamonds" : 9, "King of Diamonds" : 10, "Queen of Diamonds" : 10, "Jack of Diamonds" : 10, "Ace of Hearts" : 1, "Two of Hearts" : 2, "Three of Hearts" : 3, "Four of Hearts" : 4, "Five of Hearts" : 5, "Six of Hearts" : 6, "Seven of Hearts": 7, "Eight of Hearts" : 8, "Nine of Hearts" : 9, "King of Hearts" : 10, "Queen of Hearts" : 10, "Jack of Hearts" : 10, "Ace of Clubs" : 1, "Two of Clubs" : 2, "Three of Clubs" : 3, "Four of Clubs" : 4, "Five of Clubs" : 5, "Six of Clubs" : 6, "Seven of Clubs": 7, "Eight of Clubs" : 8, "Nine of Clubs" : 9, "King of Clubs" : 10, "Queen of Clubs" : 10, "Jack of Clubs" : 10  ]
     var body: some View {
         NavigationStack{
             ZStack {
