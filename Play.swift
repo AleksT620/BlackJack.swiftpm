982da3bf5c12aee83544dad56432aa6ba32379d4
Aleks Todorov <atodorov6421@stu.d214.org>
2025-04-29 09:50:23 -0500

    removed extra stand button

--- Play.swift
+++ Play.swift
@@ -15,9 +15,7 @@ struct PlayPage: View {
     
     var body: some View {
         VStack{
-            Button("Stand"){
-                stand()
-            }
+           
             if gameStarted && !gameOver {
                 VStack(spacing: 20) {
                     Text("Your Score: \(playerScore)")
