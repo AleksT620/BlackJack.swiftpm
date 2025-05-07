ac622999e95f3f1c721248a405d7790e85eb6d05
FrankTagoe <ftagoe6414@stu.d214.org>
2025-05-07 10:30:27 -0500

    Made it so that you could change how much you  want to bet

--- Play.swift
+++ Play.swift
@@ -55,6 +55,11 @@ class AudioManager: ObservableObject {
                                 .font(.title3)
                                 .foregroundStyle(.white)
                                 .padding(.leading)
+                            
+                            Stepper("Change bet", value: $currentBet)
+                                .frame(width: 200, height: 10, alignment: .top)
+                                .foregroundStyle(.white)
+                                .font(.headline)
                             Spacer()
                             Text("Bet: \(currentBet)")
                                 .font(.title3)
@@ -63,10 +68,13 @@ class AudioManager: ObservableObject {
                         }
                         .padding(.horizontal)
                     }
-//                    Button("Play Sound"){
-//                        audioManager.playSound(named: "WA WA WA SOUND EFFECT TRENDING SOUND EFFECT NO COPYRIGHT")
-//                    }
-//
+                    .frame(width: .infinity, height: 50, alignment: .top)
+                  
+              
+                    
+                        
+                    
+                    
                     
                     
                     if gameStarted && !gameOver {
