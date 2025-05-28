e4eb49d46d8f316bf6b1b0e3b16de4ce9d82f2c2
Aleks Todorov <atodorov6421@stu.d214.org>
2025-05-28 10:45:18 -0500

    deleted things

--- Play.swift
+++ Play.swift
@@ -1,24 +1,5 @@
 import SwiftUI
-//import Foundation
-//import AVFoundation
 
-
-
-class AudioManager: ObservableObject {
-//    var player: AVAudioPlayer?
-    
-    
-//    func playSound(named soundName: String) {
-//        if let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") {
-//            do {
-//                player = try AVAudioPlayer(contentsOf: url)
-//                player?.play()
-//            } catch {
-//                print("Error playing sound: \(error.localizedDescription)")
-//            }
-//        }
-//    }
-}
         struct PlayPage: View {
             @State var gameStarted = false
             @State var gameOver = false
